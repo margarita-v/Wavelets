c4ae197d66d1328bf4544e43bfbd9d7f921a9a42
Margarita <margo.himera@yandex.ru>
2018-12-06 12:52:00 +0300

    Small changes

--- fourier.py
+++ fourier.py
@@ -1,26 +1,31 @@
 # -*- coding: utf-8 -*- 
 
-''' УСЛОВИЕ ЗАДАЧИ:
-    Найти первые count коэффициентов Фурье для заданной функции по тригонометрическому базису. '''
+""" УСЛОВИЕ ЗАДАЧИ:
+    Найти первые count коэффициентов Фурье для заданной функции по тригонометрическому базису. """
 
 import scipy.integrate as integrate
 from math import pi, sqrt, sin, cos
 
+
 # Множитель для аргумента тригонометрических функций базиса
 def trigon_mult(k, l, x):
-    return (2*pi*k / l)*x
+    return (2 * pi * k / l) * x
+
 
 # Функция, возвращающая k-член системы тригонометрического базиса
 def f_base(x, l, k, sin_mult, cos_mult):
-    return sqrt(2 / l) * ( sin(trigon_mult(k, l, x)) * sin_mult + cos(trigon_mult(k, l, x)) * cos_mult )
+    return sqrt(2 / l) * (sin(trigon_mult(k, l, x)) * sin_mult + cos(trigon_mult(k, l, x)) * cos_mult)
+
 
 # Функция для нахождения первого элемента тригонометрического базиса
 def f_first(l):
     return sqrt(1 / l)
 
+
 # Функция для нахождения определенного интеграла заданной функции по заданным пределам интегрирования
 def integral(f, a, b):
     return integrate.quad(f, a, b)[0]
+
 
 # Функция для нахождения коэффициентов Фурье
 def fourier(f, count, l, a, b):
@@ -34,5 +39,6 @@ def fourier(f, count, l, a, b):
             result.append(integral(lambda x: f(x) * f_base(x, l, j, 1, 0), a, b))
     return result
 
+
 if __name__ == '__main__':
-    print(fourier(lambda x: x*x - 5*x, 5, 2*pi, 0, 2*pi))
+    print(fourier(lambda x: x * x - 5 * x, 5, 2 * pi, 0, 2 * pi))
--- haar.py
+++ haar.py
@@ -3,24 +3,29 @@
 import scipy.integrate as integrate
 import math
 
+
 # Базисная функция - вейвлет Хаара
 def psi(x):
-    if x >= 0 and x < .5:
+    if 0 <= x < .5:
         return 1
-    if x >= .5 and x < 1:
+    if .5 <= x < 1:
         return -1
     return 0
+
 
 # Функция, задающая систему вейвлетов Хаара
 def haar_f(x, n, k):
     return (2 ** (n * .5)) * psi((2 ** n) * x - k)
 
+
 # Функция, для которой необходимо вычислить коэффициенты Фурье по системе Хаара
 def base(x):
     return math.exp(-15 * abs(x - 0.3)) + x * x
 
+
 def get_coeff(f, a, b):
     return integrate.quad(lambda x: base(x) * f(x), a, b)[0]
+
 
 if __name__ == "__main__":
     print(get_coeff(lambda x: 1, 0, 1))
